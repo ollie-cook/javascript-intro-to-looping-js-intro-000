d1443ea7f52806efe80b000ce96955aadab256e7
Oliver Cook <olliewatford@gmail.com>
2020-06-24 13:46:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,16 @@ while (0<n){
 }
 return 'done'
 }
+
+var doWhileLoop = (num) =>{
+  let i=0;
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+} 
+
+do{
+  console.log('I run once regardless')
+} while (incrementVariable()<num)
+  
+}
